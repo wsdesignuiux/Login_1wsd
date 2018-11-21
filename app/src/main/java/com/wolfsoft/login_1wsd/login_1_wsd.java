73f223a7968102d9d1c6f5dcee956a471a80697e
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-21 10:52:54 +0530

    Initial commit

---
+++ app/src/main/java/com/wolfsoft/login_1wsd/login_1_wsd.java
@@ -0,0 +1,13 @@
+package com.wolfsoft.login_1wsd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_1_wsd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_1_wsd);
+    }
+}
